9331b2c7835f4200b09f70ea311d60cff67ff87c
Vincent Servio <vincentservio@gmail.com>
2020-03-12 22:22:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,6 +24,17 @@ attr_reader :id
     DB[:conn].execute(sql)
   end
   
+   def save
+    sql = <<-SQL
+      INSERT INTO songs (name, album)
+      VALUES (?, ?)
+    SQL
+ 
+    DB[:conn].execute(sql, self.name, self.album)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+  end
+  
+  
     def self.drop_table
               sql = <<-SQL
                 DROP TABLE  students ;
