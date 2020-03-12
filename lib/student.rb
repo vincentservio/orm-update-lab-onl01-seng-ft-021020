90044843fe08ef3ce6c56f6770f13d3cde53ac48
Vincent Servio <vincentservio@gmail.com>
2020-03-12 23:30:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,6 +57,18 @@ attr_reader :id
     student
   end
   
+   def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
+  end 
+
+   def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    DB[:conn].execute(sql, name).map { |row| new_from_db(row) }.first
+  end
+  
     def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE name = ?"
     DB[:conn].execute(sql, self.name, self.genre, self.name)
