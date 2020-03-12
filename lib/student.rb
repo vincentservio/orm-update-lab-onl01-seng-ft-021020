2f79ca367cad9e152cbca26585287fa1cba3bd7b
Vincent Servio <vincentservio@gmail.com>
2020-03-12 23:58:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,11 +20,9 @@ class Student
     DB[:conn].execute(sql)
   end 
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
 
 
-end
+
   def self.drop_table
     sql = <<-SQL
     DROP TABLE students
