4949be8e78660d268e323d40a5d7dd03a575b13b
Vincent Servio <vincentservio@gmail.com>
2020-03-12 22:12:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,15 +31,6 @@ attr_reader :id
                 DB[:conn].execute(sql)
             end
   
-  def self.create
-        sql = <<-SQL
-          CREATE TABLE IF NOT EXISTS students (
-            id INTEGER PRIMARY KEY,
-            name TEXT,
-            grade TEXT
-          )
-          SQL
-          DB[:conn].execute(sql)
-      end
+
 end
 
