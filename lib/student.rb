de59f792abe3e965a8c7ce7667793f607ed963eb
Vincent Servio <vincentservio@gmail.com>
2020-03-12 23:29:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,22 +33,19 @@ attr_reader :id
   
   
   
-   def save
-     if self.id
-    self.update
-  else
-    sql = <<-SQL
-      INSERT INTO students (name, grade)
-      VALUES (?, ?)
-    SQL
- 
-    DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL 
+        INSERT INTO students (name, grade)
+        VALUES (?,?)
+      SQL
+
+       DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
   end
-  
-  
-end
-  
   
   
   
