f756e82a4cbce39275adc9da324d43115c8ce033
Vincent Servio <vincentservio@gmail.com>
2020-03-12 22:21:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,10 +31,11 @@ attr_reader :id
                 DB[:conn].execute(sql)
             end
   
-  def self.create
+
     def self.create(name:, grade:)
     student = Student.new(name, grade)
     student.save
     student
+  end
 end
 
